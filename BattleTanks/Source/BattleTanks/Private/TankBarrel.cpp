5155cf6f95642b49be415a14faa071f870e77cdf
BuildTools <unconfigured@null.spigotmc.org>
2017-08-16 15:23:17 -0400

    Added barrel pitch movement

--- BattleTanks/Source/BattleTanks/Private/TankBarrel.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankBarrel.cpp
@@ -1,10 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankBarrel.h"
+#include "Engine/World.h"
 
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
+	auto ElevationChange = RelativeSpeed* MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	float RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	float Elevation = FMath::Clamp(RawNewElevation,MinDegreesElevation,MaxDegreesElevation);
 
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
-
